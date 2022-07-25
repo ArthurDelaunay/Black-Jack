430334032a3d107726732e470b89f4f67bfd0ad5
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-25 18:53:11 +0200

    deckCardData created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import DeckCards from "./components/DeckCards"
+import deckCardsData from "./deckCards.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      deck: [...deckCardsData],
+    }
+  }
+  render() {
+    return (
+      <main>
+        <ul>
+          {this.state.deck.map((card) => {
+            return (
+              <li key={`${card.cardName}${card.color}`}>
+                <DeckCards />
+              </li>
+            )
+          })}
+        </ul>
+      </main>
+    )
+  }
 }
-
-export default App;
