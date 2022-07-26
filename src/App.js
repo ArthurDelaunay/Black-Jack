0bc76fbbafbd3fbdfab1f6a7ef6b945c75fe20b3
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-26 14:56:00 +0200

    function distribution

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react"
 import DeckCards from "./components/DeckCards"
 import deckCardsData from "./deckCards.json"
+import CroupierSide from "./components/CroupierSide"
+import PlayerSide from "./components/PlayerSide"
+import _ from "lodash"
 import "./App.css"
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      deck: [...deckCardsData],
+      deck: _.shuffle([...deckCardsData]),
+      cardDeal: {},
     }
   }
+
+  cardDistribution = () => {
+    const clonedDeck = [...this.state.deck]
+    const cardDeal = clonedDeck.pop()
+    this.setState({
+      deck: clonedDeck,
+      cardDeal: cardDeal,
+    })
+  }
+
+  startTurn = () => {
+    this.cardDistribution(() => {})
+  }
+
   render() {
     return (
       <main>
+        <button onClick={this.startTurn}>CLick Click</button>
         {/* <ul>
           {this.state.deck.map((card) => {
             return (
@@ -22,10 +41,9 @@ class App extends Component {
             );
           })}
         </ul> */}
-        {this.state.deck.map((card) => {
-          console.log(card.imageUrl)
+        {/* {this.state.deck.map((card) => {
           return <img src={card.imageUrl} alt="" width={70} height={100} />
-        })}
+        })} */}
       </main>
     )
   }
